baa7a38f307e29c7319e6bbdc0356c5a2011865f
Jawad Hossain <jhossain@ualberta.ca>
2022-02-28 21:56:53 -0700

    Add websockets for multi client duplex communication

---
+++ README.md
@@ -0,0 +1,7 @@
+# ChatApp
+
+A Chat app bult with websockets to allow multi client - server duplex communication.
+
+## Screen Capture
+
+<img src="todo" width="800" height="381"/>
---
+++ public/js/chat.js
@@ -0,0 +1,69 @@
+const socket = io()
+
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $messages = document.querySelector('#messages')
+const $sendLocationButton = document.querySelector('#send-location')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector(
+    '#location-message-template'
+).innerHTML
+
+socket.on('message', (message) => {
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm A')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (message) => {
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.text,
+        createdAt: moment(message.createdAt).format('h:mm A')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    const message = event.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) return console.log(error)
+
+        console.log('Message Delivered')
+    })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit(
+            'sendLocation',
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            },
+            () => {
+                $sendLocationButton.removeAttribute('disabled')
+                console.log('Location shared!')
+            }
+        )
+    })
+})
